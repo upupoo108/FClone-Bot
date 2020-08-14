91b4c413c8922d6daf8ecc9f448b2b72942c0faa
Nenokkadine <65813999+Nenokkadine@users.noreply.github.com>
2020-08-14 16:47:28 +0530

    Update iCopyWeb.py

--- iCopy/iCopyWeb.py
+++ iCopy/iCopyWeb.py
@@ -4,7 +4,7 @@
 import os, sys, logging
 from multiprocessing import Process as _mp, Manager
 from web import dash
-webdash = os.environ.get("DASHBOARD")
-if webdash == true:
+webdash = int(os.environ.get("DASHBOARD"))
+if webdash:
     web = _mp(target=dash.dashboard)
     web.start()
