0a26532fdfaf425177f3eda46aa278e7a8f34e87
Nenokkadine <65813999+Nenokkadine@users.noreply.github.com>
2020-08-16 14:07:29 +0530

    Update Set.sh

--- Set.sh
+++ Set.sh
@@ -1,10 +1,38 @@
 #! /bin/bash
-
-cd /app/iCopy/config/
-echo -e "[tg]\n"token = "\"$BOT_TOKEN\"\n"usr_id = "\"$USER_ID\"\n[database]\n"db_connect_method = "\"mongodb+srv\"\n"db_addr = "\"$DB_ADDRESS\"\n"db_port = 27017"\n"db_name = "\"$DB_NAME\"\n"db_user = "\"$DB_USERNAME\"\n"db_passwd = "\"$DB_PASS\"\n[general]\n"language = "\"$LANGUAGE\"\n"cloner = "\"fclone\"\n"option = "\"$OPTION\"\n"remote = "\"icopy\"\n"parallel_c = "\"$PARALLEL_CHECKERS\"\n"parallel_t = "\"$PARALLEL_TRANSFERS\"\n"min_sleep = "\"$MIN_SLEEP\"\n"sa_path = "\"/app/iCopy/accounts\"\nrun_args = $RUN_ARGS\n[web]\ndashboard = 0\nport = 8000" >> conf.toml
-echo -e "[icopy]\ntype = drive\nscope = drive\n"service_account_file = /app/iCopy/accounts/$SA_INIT_FILE"\n"service_account_file_path = /app/iCopy/accounts/"\n"team_drive = $TEAM_DRIVE_ID"" >> rclone.conf
-cd /app/iCopy/
+cd ./iCopy/config
+echo "[tg]
+"token = "\"$BOT_TOKEN\"
+"usr_id = "\"$USER_ID\"
+[database]
+"db_connect_method = "\"$DB_CONNECT_METHOD\"
+"db_addr = "\"$DB_ADDRESS\"
+"db_port = "$DB_PORT""
+"db_name = "\"$DB_NAME\"
+"db_user = "\"$DB_USERNAME\"
+"db_passwd = "\"$DB_PASS\"
+[general]
+"language = "\"$LANGUAGE\" 
+"cloner = "\"$CLONER\"
+"option = "\"$OPTION\" 
+"remote = "\"$RCLONE_RMT\"
+"parallel_c = "\"$PARALLEL_CHECKERS\"
+"parallel_t = "\"$PARALLEL_TRANSFERS\"
+"min_sleep = "\"$MIN_SLEEP\"
+"sa_path = "\"$SA_PATH\"
+run_args = $RUN_ARGS
+[web]
+dashboard = 0
+port = 8000
+" >> conf.toml
+echo "[$RCLONE_RMT]
+type = drive
+scope = drive
+"service_account_file = /app/iCopy/accounts/$SA_INIT_FILE"
+"service_account_file_path = $SA_PATH"
+"team_drive = $FOLDER_ID"
+" >> rclone.conf
+cd ..
 wget --no-check-certificate -q $SA_ZIP_URL -O accounts.zip
 unzip -qq accounts.zip -d /app/iCopy/
-chmod +x iCopy.py
+chmod 777 iCopy.py
 python3 iCopy.py
